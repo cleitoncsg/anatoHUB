f25dcf5ebbf4dec98d239d955223b93f5eee2e70
Cleiton Gomes <cleitoncsg@gmail.com>
2015-02-08 18:27:20 -0200

    Criando a Classe Biopisa

---
+++ Anato/grails-app/domain/anato/Biopsia.groovy
@@ -0,0 +1,14 @@
+package anato
+
+class Biopsia extends Exame{
+
+	String informacoesClinicas
+	String macropsia
+	String micropsia
+	String conclusao
+	String notas
+	String legenda
+
+    static constraints = {
+    }
+}

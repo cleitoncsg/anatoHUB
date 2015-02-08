138146b57932013a3c711933f60651cfcea8fd1e
Cleiton Gomes <cleitoncsg@gmail.com>
2015-02-08 18:25:57 -0200

    Criando a Super Classe Exame

---
+++ Anato/grails-app/domain/anato/Exame.groovy
@@ -0,0 +1,14 @@
+package anato
+
+class Exame {
+
+	Date coletaDeMaterial
+	Date dataDeRequisicao
+	Date recebimentoDoMaterial
+	String materialRecebido
+	String medicoRequisitante
+	String medicoResidente
+    
+    static constraints = {
+    }
+}

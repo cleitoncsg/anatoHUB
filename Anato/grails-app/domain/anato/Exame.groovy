17b208ff449f3ec37540f7cb70e835fd2714e827
Cleiton Gomes <cleitoncsg@gmail.com>
2015-02-12 11:16:53 -0200

    Plugin Searchable

--- Anato/grails-app/domain/anato/Exame.groovy
+++ Anato/grails-app/domain/anato/Exame.groovy
@@ -2,6 +2,8 @@ package anato
 
 class Exame {
 
+	static belongsTo = [paciente: Paciente]
+
 	Date coletaDeMaterial
 	Date dataDeRequisicao
 	Date recebimentoDoMaterial
--- Anato/grails-app/domain/anato/Paciente.groovy
+++ Anato/grails-app/domain/anato/Paciente.groovy
@@ -3,7 +3,7 @@ package anato
 class Paciente {
 
 	static hasMany = [exames: Exame]
-	
+	static searchable = true
 	String nome
 	String prontuario
 	Date dataDeNascimento

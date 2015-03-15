5cbef771646227bdcc1cb50ceae3c56f4774b7bc
Vanessa Barbosa <vanessabmdf@gmail.com>
2015-03-15 07:54:28 -0300

    Editando a Classe Autopsia

--- Anato/grails-app/domain/anato/Autopsia.groovy
+++ Anato/grails-app/domain/anato/Autopsia.groovy
@@ -1,25 +1,15 @@
 package anato
 
-/**
- * Autopsia
- * A domain class describes the data object and it's mapping to the database
- */
+
 class Autopsia extends Exame{
 
-	/* Default (injected) attributes of GORM */
-//	Long	id
-//	Long	version
-	
-	/* Automatic timestamping of GORM */
-//	Date	dateCreated
-//	Date	lastUpdated
-	
 	static belongsTo = [paciente: Paciente]
-//	static	hasOne		= []	// tells GORM to associate another domain object as an owner in a 1-1 mapping
-//	static	hasMany		= []	// tells GORM to associate other domain objects for a 1-n or n-m mapping
-//	static	mappedBy	= []	// specifies which property should be used in a mapping 
 
-	String conclusao
+	String doencaPrincipal
+	String doencaConsequenciasFinal
+	String doencasContribuintes
+	String doencasConsequencia
+	String outrasDoencas
 	String notas
 	String legenda
 	
@@ -28,12 +18,5 @@ class Autopsia extends Exame{
     
 	static	constraints = {
     }
-	
-	/*
-	 * Methods of the Domain Class
-	 */
-//	@Override	// Override toString for a nicer / more descriptive UI 
-//	public String toString() {
-//		return "${name}";
-//	}
+
 }
